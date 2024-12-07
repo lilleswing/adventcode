b665a20ad2d03430f468fb5c0ac2de6f37bd6a7c
Karl Leswing <karl.leswing@gmail.com>
2024-12-07 14:13:35 -0500

    Save cookie in .env file

--- 2024/get_input_data.py
+++ 2024/get_input_data.py
@@ -15,6 +15,17 @@ def main(day, cookie):
     with open(f"day{day}.in", "w") as f:
         f.write(response.text)
 
+def read_cookie_from_env():
+    lines = open('.env').readlines()
+    for line in lines:
+        if line.startswith('COOKIE='):
+            return line.split('=')[1].strip()
+    raise ValueError("Failed to read cookie from .env")
 
 if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        cookie = read_cookie_from_env()
+        print(cookie)
+        main(sys.argv[1], cookie)
+        sys.exit(0)
     main(sys.argv[1], sys.argv[2])
